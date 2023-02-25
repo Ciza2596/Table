3a921709d5203b13df5603b125b4a2c512ad1ad6
Ciza2596 <Ciza2596@gmail.com>
2023-02-25 23:48:45 +0800

    Add PlayerPref

--- Assets/Example/Extension1 GoogleSpreadsheetLoader/Script/Editor/GoogleSpreadsheetLoaderEditor.cs
+++ Assets/Example/Extension1 GoogleSpreadsheetLoader/Script/Editor/GoogleSpreadsheetLoaderEditor.cs
@@ -1,5 +1,6 @@
 using UnityEditor;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace GoogleSpreadsheetLoader.Editor
 {
@@ -9,8 +10,31 @@ namespace GoogleSpreadsheetLoader.Editor
         private readonly string[] _toolbarTexts = { "GoogleSpreadsheetLoader", "Config" };
         private int _toolbarIndex = 0;
 
-        private string _assetPath;
-        private string _currentAssetPath;
+        private readonly string CONFIG_ASSET_PATH_KEY = $"GoogleSpreadsheetLoader.{nameof(ConfigAssetPath)}";
+
+        private string ConfigAssetPath
+        {
+            get => PlayerPrefs.GetString(CONFIG_ASSET_PATH_KEY);
+            set
+            {
+                PlayerPrefs.SetString(CONFIG_ASSET_PATH_KEY, value);
+                PlayerPrefs.Save();
+            }
+        }
+
+
+        private string CURRENT_CONFIG_ASSET_PATH_KEY = $"GoogleSpreadsheetLoader.{nameof(CurrentConfigAssetPath)}";
+
+        private string CurrentConfigAssetPath
+        {
+            get => PlayerPrefs.GetString(CURRENT_CONFIG_ASSET_PATH_KEY);
+            set
+            {
+                PlayerPrefs.SetString(CURRENT_CONFIG_ASSET_PATH_KEY, value);
+                PlayerPrefs.Save();
+            }
+        }
+
 
         private Vector2 _scrollPosition;
         private UnityEditor.Editor _googleSpreadsheetLoaderEditor;
@@ -18,7 +42,12 @@ namespace GoogleSpreadsheetLoader.Editor
 
         //private method
         [MenuItem("Tools/CizaModule/GoogleSpreadsheetLoader")]
-        private static void ShowWindow() => GetWindow<GoogleSpreadsheetLoaderEditor>("GoogleSpreadsheetLoaderEditor");
+        private static void ShowWindow() => GetWindow<GoogleSpreadsheetLoaderEditor>("GoogleSpreadsheetLoader");
+
+
+        private void OnDestroy() =>
+            PlayerPrefs.Save();
+
 
         private void OnGUI()
         {
@@ -45,10 +74,10 @@ namespace GoogleSpreadsheetLoader.Editor
 
         private void GoogleSpreadsheetLoader()
         {
-            if (_currentAssetPath != _assetPath)
+            if (CurrentConfigAssetPath != ConfigAssetPath || _googleSpreadsheetLoaderEditor is null)
             {
-                _currentAssetPath = _assetPath;
-                var googleSpreadsheetLoader = AssetDatabase.LoadAssetAtPath<Object>(_currentAssetPath);
+                CurrentConfigAssetPath = ConfigAssetPath;
+                var googleSpreadsheetLoader = AssetDatabase.LoadAssetAtPath<Object>(CurrentConfigAssetPath);
                 _googleSpreadsheetLoaderEditor = UnityEditor.Editor.CreateEditor(googleSpreadsheetLoader);
             }
 
@@ -56,15 +85,15 @@ namespace GoogleSpreadsheetLoader.Editor
                 return;
 
             EditorGUILayout.BeginHorizontal();
-            
+
             EditorGUILayout.Space(15);
-            
+
             _scrollPosition = EditorGUILayout.BeginScrollView(_scrollPosition);
             _googleSpreadsheetLoaderEditor.OnInspectorGUI();
             EditorGUILayout.EndScrollView();
-            
+
             EditorGUILayout.Space(15);
-            
+
             EditorGUILayout.EndHorizontal();
         }
 
@@ -72,7 +101,7 @@ namespace GoogleSpreadsheetLoader.Editor
         private void Config()
         {
             EditorGUILayout.Space();
-            _assetPath = GetAssetPathAndOpenWindow("Config Path", _assetPath);
+            ConfigAssetPath = GetAssetPathAndOpenWindow("Config Path", ConfigAssetPath);
             EditorGUILayout.Space();
         }
 
