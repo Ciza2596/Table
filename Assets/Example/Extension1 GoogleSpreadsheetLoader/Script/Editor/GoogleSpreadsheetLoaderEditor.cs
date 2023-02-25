56877ab73c2a5bd8a46f37b6e84e8ec1988f257e
Ciza2596 <Ciza2596@gmail.com>
2023-02-25 17:34:48 +0800

    Add GoogleSpreadsheetLoaderEditor

---
+++ Assets/Example/Extension1 GoogleSpreadsheetLoader/Script/Editor/GoogleSpreadsheetLoaderEditor.cs
@@ -0,0 +1,89 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace GoogleSpreadsheetLoader.Editor
+{
+    public class GoogleSpreadsheetLoaderEditor : EditorWindow
+    {
+        //private variable
+        private readonly string[] _toolbarTexts = { "GoogleSpreadsheetLoader", "Config" };
+        private int _toolbarIndex = 0;
+
+        private string _assetPath;
+        private string _currentAssetPath;
+
+        private Vector2 _scrollPosition;
+        private UnityEditor.Editor _googleSpreadsheetLoaderEditor;
+
+
+        //private method
+        [MenuItem("Tools/CizaModule/GoogleSpreadsheetLoader")]
+        private static void ShowWindow() => GetWindow<GoogleSpreadsheetLoaderEditor>("GoogleSpreadsheetLoaderEditor");
+
+        private void OnGUI()
+        {
+            ToolBarArea();
+
+            switch (_toolbarIndex)
+            {
+                case 0:
+                    GoogleSpreadsheetLoader();
+                    break;
+                case 1:
+                    Config();
+                    break;
+            }
+        }
+
+
+        private void ToolBarArea()
+        {
+            GUILayout.BeginHorizontal();
+            _toolbarIndex = GUILayout.Toolbar(_toolbarIndex, _toolbarTexts);
+            GUILayout.EndHorizontal();
+        }
+
+        private void GoogleSpreadsheetLoader()
+        {
+            if (_currentAssetPath != _assetPath)
+            {
+                _currentAssetPath = _assetPath;
+                var googleSpreadsheetLoader = AssetDatabase.LoadAssetAtPath<Object>(_currentAssetPath);
+                _googleSpreadsheetLoaderEditor = UnityEditor.Editor.CreateEditor(googleSpreadsheetLoader);
+            }
+
+            if (_googleSpreadsheetLoaderEditor is null)
+                return;
+            _scrollPosition = EditorGUILayout.BeginScrollView(_scrollPosition);
+            _googleSpreadsheetLoaderEditor.OnInspectorGUI();
+            EditorGUILayout.EndScrollView();
+        }
+
+
+        private void Config()
+        {
+            EditorGUILayout.Space();
+            _assetPath = GetAssetPathAndOpenWindow("Config Path", _assetPath);
+            EditorGUILayout.Space();
+        }
+
+
+        private string GetAssetPathAndOpenWindow(string label, string originPath)
+        {
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                var path = EditorGUILayout.TextField(label, originPath);
+                if (GUILayout.Button("Select", EditorStyles.miniButton, GUILayout.Width(65)))
+                {
+                    path = EditorUtility.OpenFilePanel("Folder Path", "Assets", "");
+                    var dataPath = Application.dataPath;
+                    dataPath = dataPath.Replace("Assets", "");
+                    path = path.Replace(dataPath, "");
+                }
+
+
+                return string.IsNullOrWhiteSpace(path) ? originPath : path;
+            }
+        }
+    }
+}
