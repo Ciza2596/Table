85cd4b0ce7cd70705bdf7691544fa162de26a4a2
Ciza2596 <Ciza2596@gmail.com>
2024-03-27 18:13:03 +0800

    Update Table

--- Assets/_Project/Table/Samples~/Extension1 GoogleSpreadsheetLoader/Script/Editor/CsvParser/CsvParser.cs
+++ Assets/_Project/Table/Samples~/Extension1 GoogleSpreadsheetLoader/Script/Editor/CsvParser/CsvParser.cs
@@ -1,46 +1,57 @@
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
 namespace CsvParser
 {
-	public class CsvParser
-	{
-		public string[][] Parse(string csvData)
-		{
-			var context = new ParserContext();
+    public class CsvParser
+    {
+        public string[][] Parse(string csvData)
+        {
+            var context = new ParserContext();
 
-			// Handle both Windows and Mac line endings
-			var lines = Regex.Split(csvData, "\n|\r\n");
+            // Handle both Windows and Mac line endings
+            var lines = Regex.Split(csvData, "\n|\r\n");
 
-			ParserState currentState = ParserState.LineStartState;
-			foreach (var line in lines)
-			{
-				// Skip empty entries
-				if (line.Length == 0)
-					continue;
 
-				foreach (var c in line)
-				{
-					switch (c)
-					{
-						case ParserState.COMMA_CHARACTER:
-							currentState = currentState.Comma(context);
-							break;
+            bool isInQuotation = false;
+            ParserState currentState = ParserState.LineStartState;
 
-						case ParserState.QUTOE_CHARACTER:
-							currentState = currentState.Quote(context);
-							break;
+            foreach (var line in lines)
+            {
+                // Skip empty entries
+                if (line.Length == 0)
+                    continue;
 
-						default:
-							currentState = currentState.AnyChar(c, context);
-							break;
-					}
-				}
+                foreach (var c in line)
+                {
+                    switch (c)
+                    {
+                        case ParserState.COMMA_CHARACTER:
+                            currentState = currentState.Comma(context);
+                            break;
 
-				currentState = currentState.EndOfLine(context);
-			}
+                        case ParserState.QUTOE_CHARACTER:
+                            isInQuotation = !isInQuotation;
+                            currentState = currentState.Quote(context);
+                            break;
 
-			var allLines = context.GetAllLines();
-			return allLines.ToArray();
-		}
-	}
+                        default:
+                            currentState = currentState.AnyChar(c, context);
+                            break;
+                    }
+                }
+
+                if (!isInQuotation)
+                    currentState = currentState.EndOfLine(context);
+            }
+
+            var allLines = context.GetAllLines();
+            var length = allLines[0].Length;
+            var newAllLines = new List<string[]>();
+            foreach (var line in allLines)
+                if (line.Length == length)
+                    newAllLines.Add(line);
+            return newAllLines.ToArray();
+        }
+    }
 }
