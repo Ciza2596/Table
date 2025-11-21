533e2be1ac83338a26e6d23aaff9638c56f6f22d
Ciza Huang <ciza2596@gmail.com>
2025-11-21 15:00:47 +0800

    Update ListVE

--- Assets/Plugins/Ciza/Table/Table/Script/Editor/Common/Utils/Extension/KeyExtension.cs
+++ Assets/Plugins/Ciza/Table/Table/Script/Editor/Common/Utils/Extension/KeyExtension.cs
@@ -8,5 +8,9 @@ namespace CizaTable.Editor
 		// Windows-ctrl, OSX-command
 		public static bool CheckIsCtrl(this EventModifiers modifier) =>
 			(Application.platform == RuntimePlatform.WindowsEditor && (modifier & EventModifiers.Control) != 0) || (Application.platform == RuntimePlatform.OSXEditor && (modifier & EventModifiers.Command) != 0);
+
+		// Windows-shift, OSX-shift
+		public static bool CheckIsShift(this EventModifiers modifier) =>
+			(Application.platform == RuntimePlatform.WindowsEditor && (modifier & EventModifiers.Shift) != 0) || (Application.platform == RuntimePlatform.OSXEditor && (modifier & EventModifiers.Shift) != 0);
 	}
 }
