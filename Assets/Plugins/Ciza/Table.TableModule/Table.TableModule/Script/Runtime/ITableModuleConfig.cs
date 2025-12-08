ee7965d86df7252a1867a11e95f57f9e6b08179b
Zomeraphy <zomeraphy.owo@gmail.com>
2025-12-08 16:16:53 +0800

    Update CizaUniTask

--- Assets/Plugins/Ciza/Table.TableModule/Table.TableModule/Script/Runtime/ITableModuleConfig.cs
+++ Assets/Plugins/Ciza/Table.TableModule/Table.TableModule/Script/Runtime/ITableModuleConfig.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-using Cysharp.Threading.Tasks;
+using CizaUniTask;
 
 namespace CizaTable
 {
--- Assets/Plugins/Ciza/Table.TableModule/Table.TableModule/Script/Runtime/TableModule.cs
+++ Assets/Plugins/Ciza/Table.TableModule/Table.TableModule/Script/Runtime/TableModule.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-using Cysharp.Threading.Tasks;
+using CizaUniTask;
 using UnityEngine.Scripting;
 
 namespace CizaTable
